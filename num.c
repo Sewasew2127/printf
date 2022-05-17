d55bce6b8128c47ebc5e877cae163845d872a7cb
Sewasew <emo21.sew@gmail.com>
2022-05-17 06:11:52 -0700

    betty style fixed

--- num.c
+++ num.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ *
+ * Return: nothing
+ */
 int main(void)
- {
+{
+	int num = '0';
 
-  int num = '0';
-  while (num <= '9')
-
-  {
-      putchar (num);
-      num ++;
-  }
-   putchar('\n');
-   return (0);
- }
+	while (num <= '9')
+	{
+		putchar (num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
